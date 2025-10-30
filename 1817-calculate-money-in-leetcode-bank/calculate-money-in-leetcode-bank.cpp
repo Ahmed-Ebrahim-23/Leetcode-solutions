d7be1bd96e7e3a37602d45a64e672f108e1fbeb7
Ahmed Ebrahim <95153123+Ahmed-Ebrahim-23@users.noreply.github.com>
2025-10-30 03:47:02 +0300

    Time: 0 ms (100.00%) | Memory: 7.7 MB (90.54%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int num_of_weeks = ceil(n/7.0),total=0,days;
+        int num_of_weeks = n/7 + n%7,total=0,days;
         for(int i=0;i<num_of_weeks;i++){
             days=min(7,n);
-            cout << days;
             total+= ((days+i)*(days+i+1))/2 - (i*(i+1))/2;
             n-=days;
         }
